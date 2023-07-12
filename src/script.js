1628653536f82e61fe4cc7160ccf50ad63af2da3
MellowPhi <ombt14@gmail.com>
2023-07-12 12:14:33 +1000

    added saving to localStorage functionality

---
+++ src/script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("form");
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+});
+
+
+const inputs = form.querySelectorAll('input');
+inputs.forEach(input => {
+    input.addEventListener('input', function () {
+        localStorage.setItem(input.name, input.value);
+    });
+});
+
+window.addEventListener("load", () => {
+    const inputs = form.querySelectorAll("input");
+    inputs.forEach(input => {
+        const storedValue = localStorage.getItem(input.name);
+
+        if (storedValue) {
+            input.value = storedValue
+        }
+    });
+});
