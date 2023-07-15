6893cff3864b13438089165ba090f0d71b301b27
MellowPhi <ombt14@gmail.com>
2023-07-15 10:30:25 +1000

    added history page showing added forms & updated css syles

--- src/history-script.js
+++ src/history-script.js
@@ -1,40 +1,4 @@
-// const formFieldOrder = ["first-name", "last-name", "email", "phone", "company", "address"];
-// const fieldLabels = {
-//     "first-name": "First Name",
-//     "last-name": "Last Name",
-//     "email": "Email",
-//     "phone": "Phone",
-//     "company": "Company",
-//     "address": "Address"
-// };
-// const historyCards = document.getElementsByClassName("submit-history-card");
-// window.addEventListener("load", () => {
-//     const historyCard = document.createElement("div");
-//     historyCard.setAttribute("class", "submit-history-card");
-//     formFieldOrder.forEach(key => {
-//         const storedValue = localStorage.getItem(key);
-//
-//         if (storedValue) {
-//             console.log(storedValue);
-//             const pLabel = document.createElement("label");
-//             pLabel.textContent = fieldLabels[key];
-//             historyCards[0].appendChild(pLabel);
-//
-//             const pTag = document.createElement("p");
-//             pTag.textContent = storedValue;
-//             pTag.setAttribute("class", "card-" + key);
-//             historyCards[0].appendChild(pTag);
-//         }
-//     });
-//     const deleteBtn = document.createElement("button");
-//     deleteBtn.setAttribute("class", "delete-button");
-//     deleteBtn.textContent = "Delete";
-//     historyCards[0].appendChild(deleteBtn);
-// });
-
-
-const form = document.getElementById("form");
-const historyContainer = document.getElementById("submit-history-card");
+const historyLists = document.getElementById("history-card-lists");
 const formFieldOrder = ["first-name", "last-name", "email", "phone", "company", "address"];
 const fieldLabels = {
     "first-name": "First Name",
@@ -45,8 +9,7 @@ const fieldLabels = {
     "address": "Address"
 };
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+window.addEventListener("load", () => {
     const formData = {};
 
     formFieldOrder.forEach(fieldName => {
@@ -54,44 +17,51 @@ form.addEventListener('submit', function (event) {
         formData[fieldName] = fieldValue || "";
     });
 
+
     createFormHistoryEntry(formData);
-    clearLocalStorage();
-    clearFormFields();
+})
+
+window.addEventListener("load", () => {
+    const entryList = JSON.parse(localStorage.getItem('entryList')) || [];
+    const historyLists = document.getElementById("history-card-lists");
+    //const historyContainer = document.getElementById("history-container");
+
+    entryList.forEach(entry => {
+        const formEntry = createFormHistoryEntry(entry);
+        historyLists.appendChild(formEntry);
+    });
 });
 
-function createFormHistoryEntry(formData) {
+
+function createFormHistoryEntry(entry) {
+    const listEntry = document.createElement("li");
+    listEntry.classList.add("history-card-lists");
+    listEntry.setAttribute("id", "history-card-lists");
+
     const formEntry = document.createElement("div");
-    formEntry.classList.add("form-entry");
+    formEntry.setAttribute("class", "submit-history-card");
+
 
     formFieldOrder.forEach(fieldName => {
-        const fieldLabel = document.createElement("p");
+        const fieldLabel = document.createElement("label");
         fieldLabel.textContent = fieldLabels[fieldName];
         formEntry.appendChild(fieldLabel);
 
         const fieldValue = document.createElement("p");
-        fieldValue.textContent = formData[fieldName];
+        fieldValue.textContent = entry[fieldName];
+        fieldValue.setAttribute("class", "card-" + fieldName);
         formEntry.appendChild(fieldValue);
     });
 
-    historyContainer.appendChild(formEntry);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "Delete";
+    formEntry.appendChild(deleteButton);
+
+    return formEntry;
 }
 
-function clearLocalStorage() {
-    formFieldOrder.forEach(fieldName => {
-        localStorage.removeItem(fieldName);
-    });
-}
 
-function clearFormFields() {
-    const inputs = form.querySelectorAll('input');
-    inputs.forEach(input => {
-        input.value = "";
-    });
-}
 
-window.addEventListener("load", () => {
-    const formEntries = document.getElementsByClassName("form-entry");
-    Array.from(formEntries).forEach(formEntry => {
-        formEntry.remove();
-    });
-});
+
--- src/script.js
+++ src/script.js
@@ -1,19 +1,10 @@
 const form = document.getElementById("form");
 const inputs = form.querySelectorAll('input');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const inputs = form.querySelectorAll("input");
-    inputs.forEach(input => {
-        input.value = "";
-    });
-});
-
-
-
 inputs.forEach(input => {
     input.addEventListener('input', function () {
         localStorage.setItem(input.name, input.value);
+
     });
 });
 
@@ -26,7 +17,34 @@ window.addEventListener("load", () => {
             input.value = storedValue
         }
     });
+
 });
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    saveFormEntry();
+    clearFormFields();
+});
 
+function clearFormFields() {
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.value = "";
+    });
+}
+
+function saveFormEntry() {
+    const entry = {};
+
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => {
+        const name = input.name;
+        const value = localStorage.getItem(name);
+        entry[name] = value;
+    });
+
+    const entryList = JSON.parse(localStorage.getItem('entryList')) || [];
+    entryList.push(entry);
+    localStorage.setItem('entryList', JSON.stringify(entryList));
+}
 
